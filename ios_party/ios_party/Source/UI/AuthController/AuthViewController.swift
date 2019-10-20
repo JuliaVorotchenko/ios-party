0793f68e86e92421a8b4fbb855701d301777c695
Julia Vorotchenko <juliavorotchenko@gmail.com>
2019-10-20 16:53:34 +0300

    Added auto layout for  authorization screen

---
+++ ios_party/ios_party/Source/UI/AuthController/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthViewController.swift
+//  ios_party
+//
+//  Created by Юлия Воротченко on 20.10.2019.
+//  Copyright © 2019 Юлия Воротченко. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
