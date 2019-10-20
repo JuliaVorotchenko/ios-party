6961690a81bf55b8a1f5806b52408c56a284ef1f
Julia Vorotchenko <juliavorotchenko@gmail.com>
2019-10-20 15:22:16 +0300

    initial commit

---
+++ ios_party/ios_party/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_party
+//
+//  Created by Юлия Воротченко on 20.10.2019.
+//  Copyright © 2019 Юлия Воротченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
