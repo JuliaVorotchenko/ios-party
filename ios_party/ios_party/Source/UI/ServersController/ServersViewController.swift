d6aa88585ad8a8212ae0274c4d1f8f717956a0ed
Julia Vorotchenko <juliavorotchenko@gmail.com>
2019-11-08 21:26:32 +0200

    added get servers function

--- ios_party/ios_party/Source/UI/ServersController/ServersViewController.swift
+++ ios_party/ios_party/Source/UI/ServersController/ServersViewController.swift
@@ -54,11 +54,48 @@ class ServersViewController: UIViewController, UITableViewDataSource, UITableVie
                 }
     
     @IBAction func rightBarButtonTapped(_ sender: Any) {
+        getServers()
     }
     
     @IBAction func sortButtonTapped(_ sender: Any) {
     }
     
+    
+    //MARK: Get request
+    
+    func getServers() {
+        let token = UserDefaultsContainer.sessionToken
+        
+        let url = URL(string: "http://playground.tesonet.lt/v1/servers")!
+        
+        var request = URLRequest(url: url)
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else { return }
+            print(data)
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                
+                if httpResponse.statusCode == 200 {
+                    do {
+                        let decoder = JSONDecoder()
+                        let servers = try decoder.decode([ServersModel].self, from: data)
+                        print(servers)
+                    } catch {
+                        print(error)
+                    }
+                } else if httpResponse.statusCode == 401 {
+                    print(error?.localizedDescription)
+                }
+            }
+            
+        }
+        task.resume()
+    }
 }
-
-
